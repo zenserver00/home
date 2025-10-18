a7a0d70fe775c2747acb0bece648986da5a9325d
zenserver00 <private.space.yuki@gmail.com>
2025-10-18 16:09:45 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/zen-server-website/',
+  base: '/home/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
